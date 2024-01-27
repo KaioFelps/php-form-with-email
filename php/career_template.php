b69028ff3c264e1cf888729e4e42114412b4304a
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-27 00:09:35 -0300

    init

---
+++ php/career_template.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Career proposal from <?php echo $name; ?></title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=1">
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            .content-div p {
+                margin: 4px 0 4px 0;
+                padding: 12px 16px;
+                border-radius: 24px;
+                background: #eeeeee;
+                color: #2a2a2a;
+                font-weight: normal;
+            }
+
+            .content-div p span {
+                font-weight: bold;
+                color: black;
+            }
+
+            td { word-break: break-word; }
+        </style>
+    </head>
+    <body topmargin="0" leftmargin="0" marginheight="0" marginwidth="0" background="#F9F9F9" style="margin:0;padding:0;font-family:'Helvetica Neue',Helvetica,Helvetica,Arial,sans-serif;-webkit-font-smoothing:antialiased;-webkit-text-size-adjust:none;height:100%;width:100%!important;background-color:#f9f9f9">
+        <table border="0" width="100%" cellspacing="0" cellpadding="0">
+            <tr>
+            <td>
+                <table align="center" border="0" width="100%" style="max-width:600px;" cellspacing="0" cellpadding="0">
+                    <!-- HEADER -->
+                    <tr>
+                        <td bgcolor="#1e1e1e" align="center" style="padding: 40px;">
+                            <table border="0" width="100%" cellspacing="0" cellpadding="0">
+                                <tr><td align="center"><img src="./img/logo.png" alt="Trust Corp Logo"></td></tr>
+                                <tr><td style="padding:24px 0;"><hr style="border:none;background-color:#cccccc;height:1px;"></td></tr>
+                                <tr><td align="center" style="color: white; font-size: 1.5rem;">New career proposal email from <?php echo $name; ?></td></tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <!-- BODY -->
+                    <tr>
+                        <td>
+                           <table border="1" bgcolor="#fefefe" width="100%" style="padding:24px;border-color:#1e1e1e;" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td style="border: none;">
+                                    <div class="content-div">
+                                        <p><span>Name: </span><?php echo $name; ?></p>
+                                        <p><span>Email: </span><?php echo $email; ?></p>
+                                        <p><span>Content: </span><?php echo $content; ?></p>
+                                    </div>
+                                </td>
+                            </tr>
+                           </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ php/submit-with-file.php
@@ -0,0 +1,84 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    http_response_code(404);
+    exit();
+}
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$content = $_POST["content"];
+$cv_file = $_FILES["usercvfile"];
+
+$max_file_size_in_bytes = 3145728; // 3mb in bytes
+
+if ($cv_file["size"] > $max_file_size_in_bytes || $cv_file["error"] === UPLOAD_ERR_FORM_SIZE) {
+    header('Content-Type: application/json');
+    http_response_code(400);
+
+    echo json_encode("Your file must be at most 3mb size.");
+    exit();
+}
+
+$cv_file_type = pathinfo($cv_file["name"], PATHINFO_EXTENSION);
+
+if (!in_array($cv_file_type, array("png", "pdf", "jpg", "jpeg"))) {
+    header('Content-Type: application/json');
+    http_response_code(400);
+
+    echo json_encode("Your file must be one of the types: '.png', '.pdf', '.jpg' or '.jpeg'.");
+    exit();
+}
+
+if ($cv_file["error"] !== UPLOAD_ERR_OK) {
+    header('Content-Type: application/json');
+    http_response_code(500);
+
+    echo json_encode("Error on uploading your file: ". print_r($cv_file["error"]));
+    exit();
+}
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__.'/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__.'/../vendor/phpmailer/phpmailer/src/Exception.php';
+
+$recipient_email = "myemail@gmail.com";
+$subject = "New career form from $name.";
+$cv_to_attach_path = $cv_file["tmp_name"];
+$cv_to_attach_name = $cv_file["name"];
+
+ob_start();
+include("./career_template.php");
+$body = ob_get_contents();
+ob_clean();
+
+$phpMailer = new PHPMailer();
+$phpMailer->setFrom(ini_get("sendmail_from"));
+$phpMailer->Subject= $subject;
+$phpMailer->Body = $body;
+$phpMailer->addAddress($recipient_email);
+$phpMailer->addAttachment($cv_to_attach_path, $cv_to_attach_name);
+
+$email_sent = $phpMailer->send();
+
+if(!$email_sent) {
+    header('Content-Type: application/json');
+    http_response_code(500);
+
+    echo json_encode("Internal server error.");
+    
+    file_put_contents(
+        "./console.txt",
+        PHP_EOL."$phpMailer->ErrorInfo".PHP_EOL,
+        FILE_APPEND
+
+    );
+} else {
+    header('Content-Type: application/json');
+    http_response_code(204);
+    
+    echo json_encode("Email successfully sent.");
+}
+
+?>
